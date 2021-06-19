4405b1bd69498f8dd7ba3c4bfefe96f230247e56
[dilshani] <[dilshanithathsara@gmail.com]>
2021-06-19 23:32:29 +0530

    third attempt

--- Light-home-e-commerce-web/js/script.js
+++ Light-home-e-commerce-web/js/script.js
@@ -1,6 +1,52 @@
 /*first slider*/
-$(".slider-one").slick({
+$(".slider-one")
+.not(".slick-intialized")
+.slick({
     autoplay:true,
-    autoplaySpeed:3000,
-    dots:true
+    autoplaySpeed:2000,
+    dots:true,
+
+    PrevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
 });
+
+
+/*second-slider*/
+$(".slider-two")
+.not(".slick-intialized")
+.slick({
+    PrevArrow:".slider-two .slider-btn .prev",
+    nextArrow:".slider-two .slider-btn .next",
+    slidesToShow: 5,
+  slidesToScroll: 2,
+    autoplay:true,
+    autoplaySpeed:2000,
+})
+
+/*slider 3*/
+
+$(".slider-three")
+.not(".slick-intialized")
+.slick({
+    PrevArrow:".site-slider-two .slider-btn .prev",
+    nextArrow:".site-slider-two .slider-btn .next",
+    slidesToShow: 3,
+  slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed:2000,
+})
+
+
+
+
+/*fourth-slider*/
+$(".slider-four")
+.not(".slick-intialized")
+.slick({
+    PrevArrow:".site-slider-four .slider-btn .prev",
+    nextArrow:".site-slider-four .slider-btn .next",
+    slidesToShow: 3,
+  slidesToScroll: 2,
+    autoplay:true,
+    autoplaySpeed:2000,
+})
