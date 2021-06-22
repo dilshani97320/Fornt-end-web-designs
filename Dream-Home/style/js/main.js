a4f9afce3a93992d444f379b0ce7efe434990afa
[dilshani] <[dilshanithathsara@gmail.com]>
2021-06-22 23:36:17 +0530

    java script file modified

---
+++ Dream-Home/style/js/main.js
@@ -0,0 +1,10 @@
+$(".slider-one")
+.not(".slick-intialized")
+.slick({
+    autoplay:true,
+    autoplaySpeed:2000,
+    dots:true,
+
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
+});
