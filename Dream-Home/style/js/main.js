e38e9591ffe8cb89d0be5426dd7c5373140c12a8
[dilshani] <[dilshanithathsara@gmail.com]>
2021-06-30 19:22:28 +0530

    complete js file

--- Dream-Home/style/js/main.js
+++ Dream-Home/style/js/main.js
@@ -8,3 +8,19 @@ $(".slider-one")
     prevArrow:".site-slider .slider-btn .prev",
     nextArrow:".site-slider .slider-btn .next",
 });
+
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 6,
+      slideShadows: true,
+    },
+   loop:true,
+  });
