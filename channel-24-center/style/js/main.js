e35f730e28037b365d3eb3179159588a533b4a68
[dilshani] <[dilshanithathsara@gmail.com]>
2021-07-04 23:20:38 +0530

    first attempt

---
+++ channel-24-center/style/js/main.js
@@ -0,0 +1,13 @@
+
+
+$(".slider-one")
+.not(".slick-intialized")
+.slick({
+    autoplay:true,
+    autoplaySpeed:2000,
+    dots:true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
+});
