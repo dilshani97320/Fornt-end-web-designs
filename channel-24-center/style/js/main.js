cbab082df3612614d615924d196af544a31729d4
[dilshani] <[dilshanithathsara@gmail.com]>
2021-07-06 22:07:01 +0530

    complete js file

--- channel-24-center/style/js/main.js
+++ channel-24-center/style/js/main.js
@@ -11,3 +11,18 @@ $(".slider-one")
     prevArrow:".site-slider .slider-btn .prev",
     nextArrow:".site-slider .slider-btn .next",
 });
+
+
+
+$(".slider-two")
+.not(".slick-intialized")
+.slick({
+    autoplay:true,
+    autoplaySpeed:2000,
+    dots:true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
+});
+
